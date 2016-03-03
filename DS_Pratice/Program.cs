fd21a57d95eb823a5e360b5f75a0f0dbf1ae4f73
Matthew <mnt5623@my.fsu.edu>
2016-03-02 22:13:03 -0500

    started linked list

--- DS_Pratice/Program.cs
+++ DS_Pratice/Program.cs
@@ -10,7 +10,54 @@ namespace DS_Pratice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            
+        }
+    }
+
+    public class DNode
+    {
+        static int noOfLinkedList = 0;
+        int data;
+        DNode prevNode;
+        DNode nextNode;
+
+        public DNode (int data)
+        {
+            this.data = data;
+            noOfLinkedList++;
+        }
+    }
+
+    class SinLinkedList
+    {
+        public void addNode()
+        {
+
+        }
+        public void deleteNode()
+        {
+
+        }
+
+    }
+
+    class  DoubLinkedList
+    {
+        static DNode root;
+        static DNode temp;
+        static DNode current;
+
+        public void addNode(int data)
+        {
+            DNode dNode = new DNode(data);
+        }
+        public void deleteNode()
+        {
+
+        }
+        public void insertNode()
+        {
+
         }
     }
 }
